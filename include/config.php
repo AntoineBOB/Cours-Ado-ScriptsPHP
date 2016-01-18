87be42d8a27e6b9d03456aafcb74160e677afc06
lauraBalk <laura.balk2@gmail.com>
2016-01-18 11:31:53 +0100

    parametre de connexion

--- include/config.php
+++ include/config.php
@@ -4,7 +4,7 @@
  * Database config variables
  */
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_USER", "use_stage");
+define("DB_PASSWORD", "stage_isset");
 define("DB_DATABASE", "extranet_stage");
 ?>
