94d0c962da939ab2fad864b8aa5b44ea51609915
Antoine BABU <antoinebabu85@gmail.com>
2016-01-19 09:44:31 +0100

    Hôte distant

--- include/config.php
+++ include/config.php
@@ -3,7 +3,7 @@
 /**
  * Database config variables
  */
-define("DB_HOST", "localhost");
+define("DB_HOST", "5.135.139.80");
 define("DB_USER", "use_stage");
 define("DB_PASSWORD", "stage_isset");
 define("DB_DATABASE", "extranet_stage");
